c1eb41a437bcf6386431f692bb2d079c9368ed6b
im00794 <imadzafati@gmail.com>
2020-08-17 01:10:43 +0100

    recipe list template added.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [new Recipe('Hamburger',
+    'This is a recipe to make a Hamburger',
+    'https://www.simplyhappyfoodie.com/wp-content/uploads/2018/04/instant-pot-hamburgers-3.jpg'),
+    new Recipe('Hamburger',
+      'This is a recipe to make a Hamburger',
+      'https://www.simplyhappyfoodie.com/wp-content/uploads/2018/04/instant-pot-hamburgers-3.jpg')];
 
   constructor() {
   }
