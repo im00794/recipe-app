3679b7dc7386af38190367d3b1acfacdca2a4e74
im00794 <imadzafati@gmail.com>
2020-08-31 07:34:08 +0100

    adding ingredients of recipe to shopping list.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -6,9 +6,10 @@ export class ShoppingListService {
 
   ingredientAdded = new EventEmitter<Ingredient[]>();
 
-  private ingredients: Ingredient[] = [new Ingredient('Apples', 2),
-    new Ingredient('Banana', 15),
-    new Ingredient('Orange', 7)];
+  private ingredients: Ingredient[] = [new Ingredient('apples', 2),
+    new Ingredient('banana', 15),
+    new Ingredient('orange', 7),
+    new Ingredient('meat', 20)];
 
   getIngredients() {
     return this.ingredients.slice();
@@ -16,6 +17,23 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientAdded.emit(this.ingredients);
+    this.ingredientAdded.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    ingredients.forEach((ingredient) => {
+      const indexIngredient = this.ingredients
+        .map((ingrediente) => ingrediente.name)
+        .indexOf(ingredient.name);
+      if (indexIngredient !== -1) {
+        this.ingredients[indexIngredient].amount += ingredient.amount;
+      } else {
+        this.ingredients.push(ingredient);
+      }
+    });
+    this.ingredientAdded.emit(this.ingredients.slice());
+
+    //op2
+    // this.ingredients.push(...ingredients);
   }
 }
